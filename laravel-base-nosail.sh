3af1a6605dde5fe122c34c01d9b5137819656403
Ryan West <respingitlab@trashmail.se>
2022-07-07 22:55:05 +0000

    x

--- laravel-base-nosail.sh
+++ laravel-base-nosail.sh
@@ -14,9 +14,10 @@ sudo chown -R gitpod:gitpod respins-laravel-nosail
 cd respins-laravel-nosail && sudo composer update -o --no-cache --prefer-dist && cd ../
 cd respins-laravel-nosail && php artisan jetstream:install livewire && cd ../
 sudo chown -R gitpod:gitpod respins-laravel-nosail
-cd respins-laravel-nosail && php artisan serve --host 0.0.0.0
 echo -e "Make sure to build assets using: npm install & npm run dev"
 echo
 echo -e "If you need to install npm, I suggest to use the nvm manager:"
 echo -e "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash"
 echo
+echo -e "Starting to serve HTTP using php artisan serve --host 0.0.0.0"
+cd respins-laravel-nosail && php artisan serve --host 0.0.0.0
